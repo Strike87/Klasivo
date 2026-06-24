5caa990660314a4834ae96ad323faa32f0b50b1c
Mohamed Khairy <Strike87.maths@gmail.com>
2026-06-24 23:20:28 +0300

    fix(sprint2): complete question_service org-id fix (broken commit e9f63da)
    Previous commit e9f63da only added 'organizationId' to data maps and call sites
    but forgot:
    - required String organizationId parameter to 3 method signatures
    - import organization_provider.dart to question_builder_screen
    
    Result: 9 undefined-name compile errors. This patch completes the fix.

--- lib/features/exams/pages/question_builder_screen.dart
+++ lib/features/exams/pages/question_builder_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
 import '../../../providers/question_provider.dart';
+import '../../../providers/organization_provider.dart';
 import '../../../core/config/app_constants.dart';
 import '../../../core/services/question_service.dart';
 import '../../../widgets/common_widgets.dart';
